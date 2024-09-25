1ca686341fffd5d1ad0038d36cf8f49861861409
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-09-25 16:58:28 +0300

    Use Ref

--- src/exercises 9/Ex92UncontrolledForm.tsx
+++ src/exercises 9/Ex92UncontrolledForm.tsx
@@ -1,32 +1,38 @@
 import React from 'react'
-import { useState } from 'react'
+import { useRef } from 'react'
 
 /* Develop an uncontrolled form that gathers user contact information, including first name, last name, phone number, and address. Display the entered information when the user clicks a "Submit" button */
 
 export default function Ex92UncontrolledForm() {
 
-  function formSubmitHandler(event){
+  const firstNameRef = useRef<HTMLInputElement>(null);
+
+  // function formSubmitHandler(event){
+  //   event.preventDefault();
+  //   console.log('Form submitted')
+
+  //   //read the form data from the form fields
+  //   const formData = new FormData(event.target)
+
+  //   //print the form values to console one by one
+
+  //   console.log('First Name:', formData.get('firstName'));
+  //   console.log('Last Name:', formData.get('lastName'));
+  //   console.log('Email:', formData.get('email'));
+  //   console.log('Password:', formData.get('pwd'));
+
+  function formSubmitWithRefExample(event){
     event.preventDefault();
-    console.log('Form submitted')
-
-    //read the form data from the form fields
-    const formData = new FormData(event.target)
-
-    //print the form values to console one by one
-
-    console.log('First Name:', formData.get('firstName'));
-    console.log('Last Name:', formData.get('lastName'));
-    console.log('Email:', formData.get('email'));
-    console.log('Password:', formData.get('pwd'));
-
+    console.log('First name:', firstNameRef.current.value)
+    
   }
 
   return (
     <div>
       <h1>Exercise 92. Uncontrolled form</h1>
-      <form onSubmit={formSubmitHandler}>
+      <form onSubmit={formSubmitWithRefExample}>
         <div>
-          <input type="text"  name='firstName' placeholder='First Name' />
+          <input type="text" ref={firstNameRef} name='firstName' placeholder='First Name' />
         </div>
         <div>
           <input type="text" name='lastName' placeholder='Last Name' />
